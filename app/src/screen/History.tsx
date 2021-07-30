da3157b3afade5aa3c173d85b3df60698512f868
brkss <berkassebrahim@gmail.com>
2021-07-30 17:13:23 +0100

    ⚡️ APP : REFRESH

--- app/src/screen/History.tsx
+++ app/src/screen/History.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet, ScrollView, Dimensions} from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Dimensions, RefreshControl} from 'react-native';
 import { Element, TopBar } from '../component';
 import { useHistoryQuery } from '../generated/graphql';
 
-
+const wait = (timeout: number) => {
+    return new Promise(resolve => {
+      setTimeout(resolve, timeout);
+    });
+};
 const { height } = Dimensions.get('window');
 
 export const History : React.FC = (props: any) => {
 
-    const _history = useHistoryQuery();
+    const _history = useHistoryQuery({
+        notifyOnNetworkStatusChange: true
+    });
+    const [refreshing, setRefreshing] = React.useState(false);
+    
+    // On page refresh !!
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+        _history.refetch()
+        wait(2000).then(() => setRefreshing(false));
+    }, []);
 
     if(_history.loading){
         return(
@@ -24,7 +38,9 @@ export const History : React.FC = (props: any) => {
                 <TopBar onClick={(screen: string) => props.navigation.navigate(screen)} />
             </View>
             <View style={styles.content}>
-                <ScrollView showsVerticalScrollIndicator={false}>
+                <ScrollView showsVerticalScrollIndicator={false}
+                    refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+                >
                     {
                         _history.data?.history.map((history, key) => (
                             <Element {...{key}} bg_before={history.bg_before} carbs={history.carbs} date={history.created_at} />        
