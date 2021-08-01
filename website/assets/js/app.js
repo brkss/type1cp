b005d7934d148c9f72e708f7cfacd5877c3c478e
brkss <berkassebrahim@gmail.com>
2021-08-01 16:15:17 +0100

    ⚡️ WEBSITE : REMOVE SCROLL

--- website/assets/js/app.js
+++ website/assets/js/app.js
@@ -1,9 +1,26 @@
 
 
+let currentScrool = 0;
+let scrollTimer = null;
+const pageHeight = window.innerHeight;
 
 
 window.addEventListener('scroll', (e) => {
-  
-  console.log("scroll event -> ", e);
+
+  if(scrollTimer == null){
+    clearTimeout(scrollTimer);
+  }
+  scrollTimer = setTimeout(() => {
+    console.log("scroll ended !");
+      
+    if(window.scrollY > currentScrool + 1){
+      currentScrool += pageHeight;
+      window.scrollBy(0, currentScrool);
+    }else {
+      currentScrool -= pageHeight;
+      window.scrollBy(0, currentScrool);
+    }
+
+  }, 500);
 
 })
